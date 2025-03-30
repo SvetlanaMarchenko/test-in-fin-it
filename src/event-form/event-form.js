25e65c2347bbe51777b0347ca97e3e4d80d15bd2
SvetlanaMarchenko <semusv@mail.ru>
2025-03-30 11:55:10 +0300

    Now json file is changing the data. WIP

--- src/event-form/event-form.js
+++ src/event-form/event-form.js
@@ -1,5 +1,5 @@
 import './event-form.css'; 
-import React, { useState } from 'react';
+import React from 'react';
 import { Box, Stack, TextField } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 
@@ -9,19 +9,42 @@ const fields = [
   { label: 'Компания', valueKey: 'company' },
 ];
 
-export default function EventForm({selectedItem}) {
-  const [ setFormData] = useState({
-    jobTitle: '',
-    department: '',
-    company: '',
-  });
+export default function EventForm({selectedItem, setSelectedItem}) {
+  // const [setFormData] = useState({
+  //   jobTitle: '',
+  //   department: '',
+  //   company: '',
+  // });
+  // const [isChanged, setIsChanged] = useState(false); 
 
   const handleChange = (event, key) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      [key]: event.target.value,
-    }));
+    const updatedItem = { ...selectedItem, [key]: event.target.value }
+    setSelectedItem(updatedItem) 
+    handleUpdate(updatedItem)
   };
+
+  const handleUpdate = async (updatedItem) => {
+    if (!updatedItem?.id) return;
+
+    try {
+      const response = await fetch(`http://localhost:3002/users/${updatedItem.id}`, {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ [Object.keys(updatedItem).pop()]: Object.values(updatedItem).pop() }),
+      });
+
+      if (!response.ok) {
+      throw new Error('Не удается обновить данные');
+      }
+      } catch (error) {
+        console.error('Ошибка:', error);
+      }
+      };
+
+  // const handleCancel = () => {
+  //   setFormData('')
+  //   setIsChanged(false);
+  // };
 
   return (
     <Box>
@@ -45,6 +68,19 @@ export default function EventForm({selectedItem}) {
               />
             </div>
           ))}
+          {/* {isChanged && (
+                <div>
+                  <Button
+                    type="secondary"
+                    size="large"
+                    onClick={handleCancel}
+                    htmlType="reset"
+                  >
+                    Отмена
+                  </Button>
+                  <Button type="primary" size="large" htmlType="submit">Сохранить</Button>
+                </div>
+              )} */}
         </Stack>
       </Box>
     </Box>
